a0371286936e819429fd2d4a83044885d7f1598c
Maya Murad <mmurad7@uwo.ca>
2020-12-07 19:50:52 -0500

    fixed buttons

--- src/RoutesButton.jsx
+++ src/RoutesButton.jsx
@@ -5,7 +5,7 @@ class MenuButton extends Component{
     render() {
         return (
             <button id="BusRoutesButton" 
-            style = {{width: 160, height: 30}}
+            style = {{width: 160, height: 60}}
             onClick= {this.props.handleClick}>
                 Bus Routes</button>
         );
