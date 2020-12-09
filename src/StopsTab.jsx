ad8e2a9b16ce27ef659356b4a83ba2c9202cb599
Ryan Davis <rdavis54@uwo.ca>
2020-12-08 22:39:08 -0500

    Added bus icon to bus stop markers

--- src/StopsTab.jsx
+++ src/StopsTab.jsx
@@ -3,7 +3,7 @@ import Map from './map.jsx'
 import { stopsJson } from './stops.js';
 import { Marker } from '@react-google-maps/api';
 import './StopsTab.css';
-
+import bus_pin from './images/bus_pin.png';
 var stopValues = {}
 var markers = []
 class StopsTab extends Component 
@@ -16,7 +16,7 @@ class StopsTab extends Component
             var lat = stopValues[keyVal][0]['stop_lat']
             var long = stopValues[keyVal][0]['stop_lon']
             markers.push(
-                <Marker key = {keyVal} position={{'lat':lat,'lng':long}} onClick = {this.props.proccessStop.bind(this,stopValues[keyVal])}/>
+                <Marker key = {keyVal} position={{'lat':lat,'lng':long}} icon = { bus_pin } onClick = {this.props.proccessStop.bind(this,stopValues[keyVal])}/>
                 )
         }
         this.props.handleStopsClicked(markers);
